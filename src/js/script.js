bd24cad720b4809674669ff330abdee53842bcf5
caio frota <caioalvesfrota@gmail.com>
2026-09-20 18:40:02 -0300

    Finaliza layout e identidade visual da página

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,21 @@
 var nome = prompt("Qual é o nome do vinho?");
+
 var tipo = prompt("Qual é o tipo do vinho? Tinto, Branco ou Rosé?");
+
 var safra = prompt("Qual é o ano da safra?");
+
 var quantidade = prompt("Quantas unidades há em estoque?");
 
 alert("Cadastro realizado! Veja os detalhes no console.");
 
+alert("A seguir, veja os detalhes do vinho no console.");
+
 console.log("=== VINHO CADASTRADO ===");
+
 console.log("Nome: " + nome);
+
 console.log("Tipo: " + tipo);
+
 console.log("Safra: " + safra);
+
 console.log("Quantidade em estoque: " + quantidade);
